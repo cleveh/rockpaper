9303c9e2cbdb08fbd27e4fda39f911385a7b2c7b
chjyeow <yeow.hanjiang97@gmail.com>
2022-01-26 00:50:09 +0800

    Made changes to rock paper scissors to include a "ui" incomplete

--- rock/script.js
+++ rock/script.js
@@ -2,34 +2,54 @@ let playerScore = 0;
 let computerScore = 0;
 function computerPlay() {
     let num = Math.floor(Math.random()* 3);
-    return ["rock", "paper", "scissors"][num];
+    const compChoice = ["Rock", "Paper", "Scissors"][num];
+    document.querySelector(".comp-choice").innerText = `Computer plays ${compChoice}`;
+    return compChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
+    document.querySelector(".player-choice").innerText = `Player plays ${playerSelection}`;
     if (playerSelection == computerSelection) {
-        return "It's a tie!"
+        document.querySelector(".results").innerHTML = "It's a tie!"
+        return;
     }
-    let player = playerSelection.toLowerCase();
-    if (player == "rock") return computerSelection == "paper" ? computerWin(player, computerSelection) : playerWin(player, computerSelection);
-    if (player == "paper") return computerSelection == "scissors" ? computerWin(player, computerSelection) : playerWin(player, computerSelection);
-    if (player == "scissors") return computerSelection == "rock" ? computerWin(player, computerSelection) : playerWin(player, computerSelection);
+    let player=playerSelection;
+    if (player == "Rock") computerSelection == "Paper" ? computerWin(player, computerSelection) : playerWin(player, computerSelection);
+    if (player == "Paper") computerSelection == "Scissors" ? computerWin(player, computerSelection) : playerWin(player, computerSelection);
+    if (player == "Scissors") computerSelection == "Rock" ? computerWin(player, computerSelection) : playerWin(player, computerSelection);
+    document.querySelector(".score").innerText = `Player: ${playerScore} Computer: ${computerScore}`;
 } 
 
 function game() {
-    let playerSelection = prompt("What do you play?")
-    console.log(playRound(playerSelection, computerPlay()));
-    console.log("You: " + playerScore + " " + "Computer: " + computerScore);
-    let playAgain = prompt("Play again?");
-    if (playAgain.toLowerCase() == "yes") {
-        game();
-    }
+    document.querySelector(".text").innerText = "What do you play?"
+    const choice = document.querySelector(".choice");
+    choice.appendChild(div);
 }
+
 function playerWin(playerSelection, computerSelection) {
     playerScore += 1;
-    return `You win! ${playerSelection} beats ${computerSelection}`;
+    document.querySelector(".results").innerHTML =`You win! ${playerSelection} beats ${computerSelection}`;
 }
 
 function computerWin(playerSelection, computerSelection) {
     computerScore += 1;
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
+    document.querySelector(".results").innerHTML = `You lose! ${computerSelection} beats ${playerSelection}`;
 }
+
+const start = document.querySelector(".start");
+start.addEventListener("click", game, {once:"true"});
+
+const div = document.createElement("div");
+const rock = document.createElement("button");
+rock.innerHTML = "Rock";
+const scissors = document.createElement("button");
+scissors.innerHTML = "Scissors";
+const paper = document.createElement("button");
+paper.innerHTML = "Paper";
+div.appendChild(rock);
+div.appendChild(paper);
+div.appendChild(scissors);
+
+scissors.addEventListener("click", () => playRound("Scissors", computerPlay() ));
+rock.addEventListener("click", () => playRound("Rock", computerPlay() ));
+paper.addEventListener("click", () => playRound("Paper", computerPlay() ));
